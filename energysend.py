a68968432c4be4e13eed49ad01afb05454890040
James Martinez <james-martinez@users.noreply.github.com>
2014-07-22 13:40:36 -0500

    Create energysend.py
    used example python script to send data. needs to be tested

---
+++ energysend.py
@@ -0,0 +1,39 @@
+import serial, sys
+import httplib
+
+# Domain you want to post to: localhost would be an emoncms installation on your own laptop
+# this could be changed to emoncms.org to post to emoncms.org
+domain = "emoncms.org"
+
+# Location of emoncms in your server, the standard setup is to place it in a folder called emoncms
+# To post to emoncms.org change this to blank: ""
+emoncmspath = ""
+
+# Write apikey of emoncms account
+apikey = "b742b99d9880b27ff50ce73b17c2e224"
+
+# Node id youd like the emontx to appear as
+nodeid = 1
+
+conn = httplib.HTTPConnection(domain)
+
+while 1:
+
+  # Read in line of readings from emontx serial
+  linestr = ser.readline()
+
+  # Remove the new line at the end
+  linestr = linestr.rstrip()
+
+  print linestr
+
+  # Split the line at the whitespaces
+  array = linestr.split(' ')
+
+  # Create csv string
+  csv = ",".join(array)
+
+  # Send to emoncms
+  conn.request("GET", "/"+emoncmspath+"/input/post.json?apikey="+apikey+"&node="+str(nodeid)+"&csv="+csv)
+  response = conn.getresponse()
+  print response.read()
